b2ca5da2ecb3faf84014df19a9a455bc7a65ded5
xxAROX <arox.email.0@gmail.com>
2020-06-30 11:57:15 +0200

    added LabTableHeatblockDustParticle

---
+++ xxAROX/BetterParticles/LabTableHeatblockDustParticle.php
@@ -0,0 +1,13 @@
+<?php
+/* Copyright (c) 2020 xxAROX. All rights reserved. */
+namespace xxAROX\BetterParticles;
+/**
+ * Class LabTableHeatblockDustParticle
+ * @package xxAROX\BetterParticles
+ * @author xxAROX
+ * @date 30.06.2020 - 11:56
+ * @project BetterParticles
+ */
+class LabTableHeatblockDustParticle extends BetterParticle{
+	const TYPE_ID = "minecraft:lab_table_heatblock_dust_particle";
+}

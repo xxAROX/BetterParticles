65d8adf619654814d018b72862aa9f6c692ea80f
xxAROX <arox.email.0@gmail.com>
2020-06-30 12:07:26 +0200

    added WitherBossInvulnerableParticle

---
+++ xxAROX/BetterParticles/WitherBossInvulnerableParticle.php
@@ -0,0 +1,13 @@
+<?php
+/* Copyright (c) 2020 xxAROX. All rights reserved. */
+namespace xxAROX\BetterParticles;
+/**
+ * Class WitherBossInvulnerableParticle
+ * @package xxAROX\BetterParticles
+ * @author xxAROX
+ * @date 30.06.2020 - 12:07
+ * @project BetterParticles
+ */
+class WitherBossInvulnerableParticle extends BetterParticle{
+	const TYPE_ID = "minecraft:wither_boss_invulnerable";
+}

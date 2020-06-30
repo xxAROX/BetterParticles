0e654dff9f168f484ec19e5b7966fae320ee6712
xxAROX <arox.email.0@gmail.com>
2020-06-30 11:52:54 +0200

    added DragonBreathParticle

---
+++ xxAROX/BetterParticles/DragonBreathParticle.php
@@ -0,0 +1,13 @@
+<?php
+/* Copyright (c) 2020 xxAROX. All rights reserved. */
+namespace xxAROX\BetterParticles;
+/**
+ * Class DragonBreathParticle
+ * @package xxAROX\BetterParticles
+ * @author xxAROX
+ * @date 30.06.2020 - 11:52
+ * @project BetterParticles
+ */
+class DragonBreathParticle extends BetterParticle{
+	const TYPE_ID = "minecraft:dragon_breath_fire";
+}

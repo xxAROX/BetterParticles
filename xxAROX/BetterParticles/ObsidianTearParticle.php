e48ef01f3f53c0a92d9d8e92750e65522f4700c9
xxAROX <arox.email.0@gmail.com>
2020-06-30 11:30:54 +0200

    added ObsidianTearParticle

---
+++ xxAROX/BetterParticles/ObsidianTearParticle.php
@@ -0,0 +1,13 @@
+<?php
+/* Copyright (c) 2020 xxAROX. All rights reserved. */
+namespace xxAROX\BetterParticles;
+/**
+ * Class ObsidianTearParticle
+ * @package xxAROX\BetterParticles
+ * @author xxAROX
+ * @date 30.06.2020 - 11:30
+ * @project BetterParticles
+ */
+class ObsidianTearParticle extends BetterParticle{
+	const TYPE_ID = "minecraft:obsidian_tear_particle";
+}

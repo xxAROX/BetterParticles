515cf86cff685e8ad5a81c3fd148ca44f327e2c4
xxAROX <arox.email.0@gmail.com>
2020-06-30 11:45:26 +0200

    added CauldronExplosionParticle

---
+++ xxAROX/BetterParticles/CauldronExplosionParticle.php
@@ -0,0 +1,13 @@
+<?php
+/* Copyright (c) 2020 xxAROX. All rights reserved. */
+namespace xxAROX\BetterParticles;
+/**
+ * Class CauldronExplosionParticle
+ * @package xxAROX\BetterParticles
+ * @author xxAROX
+ * @date 30.06.2020 - 11:45
+ * @project BetterParticles
+ */
+class CauldronExplosionParticle extends BetterParticle{
+	const TYPE_ID = "minecraft:cauldron_explosion_emitter";
+}

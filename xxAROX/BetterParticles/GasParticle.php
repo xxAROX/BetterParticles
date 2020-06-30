ec1be1c5fef1752dd96f652cb9a35f2a51430e54
xxAROX <arox.email.0@gmail.com>
2020-06-30 11:42:36 +0200

    added GasParticle

---
+++ xxAROX/BetterParticles/GasParticle.php
@@ -0,0 +1,13 @@
+<?php
+/* Copyright (c) 2020 xxAROX. All rights reserved. */
+namespace xxAROX\BetterParticles;
+/**
+ * Class GasParticle
+ * @package xxAROX\BetterParticles
+ * @author xxAROX
+ * @date 30.06.2020 - 11:42
+ * @project BetterParticles
+ */
+class GasParticle extends BetterParticle{
+	const TYPE_ID = "minecraft:balloon_gas_particle";
+}

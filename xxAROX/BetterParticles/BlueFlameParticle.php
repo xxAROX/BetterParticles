0568b2eb4022a264831bce97ae4ed8d42dcd1eba
xxAROX <arox.email.0@gmail.com>
2020-06-30 11:02:57 +0200

    Added BlueFlameParticle.php

---
+++ xxAROX/BetterParticles/BlueFlameParticle.php
@@ -0,0 +1,15 @@
+<?php
+/* Copyright (c) 2020 xxAROX. All rights reserved. */
+namespace xxAROX\BetterParticles;
+
+
+/**
+ * Class BlueFlameParticle
+ * @package xxAROX\BetterParticles
+ * @author xxAROX
+ * @date 30.06.2020 - 10:43
+ * @project BetterParticles
+ */
+class BlueFlameParticle extends BetterParticle{
+	const TYPE_ID = "minecraft:blue_flame_particle";
+}

bccaef48e72fce4d677b71ffe2d548e90dd9d860
xxAROX <arox.email.0@gmail.com>
2020-06-30 11:44:11 +0200

    added CampfireParticle

---
+++ xxAROX/BetterParticles/CampfireParticle.php
@@ -0,0 +1,13 @@
+<?php
+/* Copyright (c) 2020 xxAROX. All rights reserved. */
+namespace xxAROX\BetterParticles;
+/**
+ * Class CampfireParticle
+ * @package xxAROX\BetterParticles
+ * @author xxAROX
+ * @date 30.06.2020 - 11:43
+ * @project BetterParticles
+ */
+class CampfireParticle extends BetterParticle{
+	const TYPE_ID = "minecraft:campfire_tall_smoke_particle";
+}

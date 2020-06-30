9ef3271b66253796c3d783fb91680fc4a271c9c2
xxAROX <arox.email.0@gmail.com>
2020-06-30 11:33:31 +0200

    added SoulParticle

---
+++ xxAROX/BetterParticles/SoulParticle.php
@@ -0,0 +1,13 @@
+<?php
+/* Copyright (c) 2020 xxAROX. All rights reserved. */
+namespace xxAROX\BetterParticles;
+/**
+ * Class SoulParticle
+ * @package xxAROX\BetterParticles
+ * @author xxAROX
+ * @date 30.06.2020 - 11:33
+ * @project BetterParticles
+ */
+class SoulParticle extends BetterParticle{
+	const TYPE_ID = "minecraft:soul_particle";
+}

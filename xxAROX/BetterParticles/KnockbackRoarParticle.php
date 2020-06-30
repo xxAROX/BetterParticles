cca7733e6d9f600773341023a905c98dc683617d
xxAROX <arox.email.0@gmail.com>
2020-06-30 11:58:07 +0200

    added KnockbackRoarParticle

---
+++ xxAROX/BetterParticles/KnockbackRoarParticle.php
@@ -0,0 +1,13 @@
+<?php
+/* Copyright (c) 2020 xxAROX. All rights reserved. */
+namespace xxAROX\BetterParticles;
+/**
+ * Class KnockbackRoarParticle
+ * @package xxAROX\BetterParticles
+ * @author xxAROX
+ * @date 30.06.2020 - 11:57
+ * @project BetterParticles
+ */
+class KnockbackRoarParticle extends BetterParticle{
+	const TYPE_ID = "minecraft:knockback_roar_particle";
+}

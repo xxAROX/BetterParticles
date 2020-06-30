6f1f44995c403290f14aa73f3f16c60adda17ac3
xxAROX <arox.email.0@gmail.com>
2020-06-30 11:47:17 +0200

    added ConduitAbsorbParticle

---
+++ xxAROX/BetterParticles/ConduitAbsorbParticle.php
@@ -0,0 +1,13 @@
+<?php
+/* Copyright (c) 2020 xxAROX. All rights reserved. */
+namespace xxAROX\BetterParticles;
+/**
+ * Class ConduitAbsorbParticle
+ * @package xxAROX\BetterParticles
+ * @author xxAROX
+ * @date 30.06.2020 - 11:47
+ * @project BetterParticles
+ */
+class ConduitAbsorbParticle extends BetterParticle{
+	const TYPE_ID = "minecraft:conduit_absorb_particle";
+}

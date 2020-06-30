a6e2793f1c64d0bf5f2f58084e57a225a8e065cd
xxAROX <arox.email.0@gmail.com>
2020-06-30 11:53:52 +0200

    added DragonDeathExplosionParticle

---
+++ xxAROX/BetterParticles/DragonDeathExplosionParticle.php
@@ -0,0 +1,13 @@
+<?php
+/* Copyright (c) 2020 xxAROX. All rights reserved. */
+namespace xxAROX\BetterParticles;
+/**
+ * Class DragonDeathExplosionParticle
+ * @package xxAROX\BetterParticles
+ * @author xxAROX
+ * @date 30.06.2020 - 11:53
+ * @project BetterParticles
+ */
+class DragonDeathExplosionParticle extends BetterParticle{
+	const TYPE_ID = "minecraft:dragon_death_explosion_emitter";
+}

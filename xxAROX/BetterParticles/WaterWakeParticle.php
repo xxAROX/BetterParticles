f5a774c276b8f9908fca5690799618a81f0f843a
xxAROX <arox.email.0@gmail.com>
2020-06-30 12:06:16 +0200

    added WaterWakeParticle

---
+++ xxAROX/BetterParticles/WaterWakeParticle.php
@@ -0,0 +1,13 @@
+<?php
+/* Copyright (c) 2020 xxAROX. All rights reserved. */
+namespace xxAROX\BetterParticles;
+/**
+ * Class WaterWakeParticle
+ * @package xxAROX\BetterParticles
+ * @author xxAROX
+ * @date 30.06.2020 - 12:06
+ * @project BetterParticles
+ */
+class WaterWakeParticle extends BetterParticle{
+	const TYPE_ID = "minecraft:water_wake_particle";
+}

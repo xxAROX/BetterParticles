5a2264fc524652529e7d66e0a6e852fd53b629ec
xxAROX <arox.email.0@gmail.com>
2020-06-30 11:41:32 +0200

    added BleachParticle

---
+++ xxAROX/BetterParticles/BleachParticle.php
@@ -0,0 +1,13 @@
+<?php
+/* Copyright (c) 2020 xxAROX. All rights reserved. */
+namespace xxAROX\BetterParticles;
+/**
+ * Class BleachParticle
+ * @package xxAROX\BetterParticles
+ * @author xxAROX
+ * @date 30.06.2020 - 11:41
+ * @project BetterParticles
+ */
+class BleachParticle extends BetterParticle{
+	const TYPE_ID = "minecraft:bleach";
+}

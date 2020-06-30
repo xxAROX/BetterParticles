993bcb7a9db105352f009aba21c241dee3318914
xxAROX <arox.email.0@gmail.com>
2020-06-30 11:32:26 +0200

    added ShulkerBulletParticle

---
+++ xxAROX/BetterParticles/ShulkerBulletParticle.php
@@ -0,0 +1,13 @@
+<?php
+/* Copyright (c) 2020 xxAROX. All rights reserved. */
+namespace xxAROX\BetterParticles;
+/**
+ * Class ShulkerBulletParticle
+ * @package xxAROX\BetterParticles
+ * @author xxAROX
+ * @date 30.06.2020 - 11:32
+ * @project BetterParticles
+ */
+class ShulkerBulletParticle extends BetterParticle{
+	const TYPE_ID = "minecraft:shulker_bullet";
+}

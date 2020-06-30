180fd848cc1ff31b632753c133792a0502b4f67c
xxAROX <arox.email.0@gmail.com>
2020-06-30 12:29:20 +0200

    Now its a Plugin

---
+++ src/xxAROX/BetterParticles/Main.php
@@ -0,0 +1,21 @@
+<?php
+/* Copyright (c) 2020 xxAROX. All rights reserved. */
+namespace xxAROX\BetterParticles;
+use pocketmine\plugin\PluginBase;
+
+
+/**
+ * Class Main
+ * @package xxAROX\BetterParticles
+ * @author xxAROX
+ * @date 30.06.2020 - 12:27
+ * @project BetterParticles
+ */
+class Main extends PluginBase{
+	public function onLoad(): void{
+	}
+	public function onEnable(): void{
+	}
+	public function onDisable(): void{
+	}
+}

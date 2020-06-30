7d4e8d6365f5c3ae9e3b526f14d6aa08e7d3e2fc
xxAROX <arox.email.0@gmail.com>
2020-06-30 11:59:35 +0200

    added LlamaSpitSmokeParticle

---
+++ xxAROX/BetterParticles/LlamaSpitSmokeParticle.php
@@ -0,0 +1,13 @@
+<?php
+/* Copyright (c) 2020 xxAROX. All rights reserved. */
+namespace xxAROX\BetterParticles;
+/**
+ * Class LlamaSpitSmokeParticle
+ * @package xxAROX\BetterParticles
+ * @author xxAROX
+ * @date 30.06.2020 - 11:59
+ * @project BetterParticles
+ */
+class LlamaSpitSmokeParticle extends BetterParticle{
+	const TYPE_ID = "minecraft:llama_spit_smoke";
+}

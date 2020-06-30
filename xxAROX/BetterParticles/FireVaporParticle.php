c1ef09103718e331f0081b97d14a6962e9f6cc03
xxAROX <arox.email.0@gmail.com>
2020-06-30 12:02:59 +0200

    added FireVaporParticle

---
+++ xxAROX/BetterParticles/FireVaporParticle.php
@@ -0,0 +1,13 @@
+<?php
+/* Copyright (c) 2020 xxAROX. All rights reserved. */
+namespace xxAROX\BetterParticles;
+/**
+ * Class FireVaporParticle
+ * @package xxAROX\BetterParticles
+ * @author xxAROX
+ * @date 30.06.2020 - 12:02
+ * @project BetterParticles
+ */
+class FireVaporParticle extends BetterParticle{
+	const TYPE_ID = "minecraft:misc_fire_vapor_particle";
+}

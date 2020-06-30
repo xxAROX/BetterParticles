0d5dd578438dda90385cb8c21b415e8acb4d0cc1
xxAROX <arox.email.0@gmail.com>
2020-06-30 11:36:54 +0200

    added TotemParticle

---
+++ xxAROX/BetterParticles/TotemParticle.php
@@ -0,0 +1,13 @@
+<?php
+/* Copyright (c) 2020 xxAROX. All rights reserved. */
+namespace xxAROX\BetterParticles;
+/**
+ * Class TotemParticle
+ * @package xxAROX\BetterParticles
+ * @author xxAROX
+ * @date 30.06.2020 - 11:36
+ * @project BetterParticles
+ */
+class TotemParticle extends BetterParticle{
+	const TYPE_ID = "minecraft:totem_particle";
+}

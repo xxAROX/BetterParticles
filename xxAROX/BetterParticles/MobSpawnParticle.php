0eb9645f34ad9444078697a995b7f30ba848c04e
xxAROX <arox.email.0@gmail.com>
2020-06-30 12:03:35 +0200

    added MobSpawnParticle

---
+++ xxAROX/BetterParticles/MobSpawnParticle.php
@@ -0,0 +1,13 @@
+<?php
+/* Copyright (c) 2020 xxAROX. All rights reserved. */
+namespace xxAROX\BetterParticles;
+/**
+ * Class MobSpawnParticle
+ * @package xxAROX\BetterParticles
+ * @author xxAROX
+ * @date 30.06.2020 - 12:03
+ * @project BetterParticles
+ */
+class MobSpawnParticle extends BetterParticle{
+	const TYPE_ID = "minecraft:mob_block_spawn_emitter";
+}

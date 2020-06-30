08859d56aa3c7c0c28643ec5e5a33fbea6dc57ba
xxAROX <arox.email.0@gmail.com>
2020-06-30 12:02:07 +0200

    added MagnesiumSaltsParticle

---
+++ xxAROX/BetterParticles/MagnesiumSaltsParticle.php
@@ -0,0 +1,13 @@
+<?php
+/* Copyright (c) 2020 xxAROX. All rights reserved. */
+namespace xxAROX\BetterParticles;
+/**
+ * Class MagnesiumSaltsParticle
+ * @package xxAROX\BetterParticles
+ * @author xxAROX
+ * @date 30.06.2020 - 12:01
+ * @project BetterParticles
+ */
+class MagnesiumSaltsParticle extends BetterParticle{
+	const TYPE_ID = "minecraft:magnesium_salts_emitter";
+}
